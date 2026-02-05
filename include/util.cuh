1f5b165ac32ba768ab55990839600944188b4042
lizardo-andres12 <lizardo.andres12@gmail.com>
2026-02-04 20:29:35 -0500

    [util] add device pointer assertion

--- include/util.cuh
+++ include/util.cuh
@@ -1,7 +1,9 @@
 #pragma once
 
+#include <cuda_runtime.h>
 #include <iostream>
 
+// CUDA error check passes if there was no error and force quits the program if there was an error
 void checkCudaErrors(cudaError_t err, const char* file, int line) {
     if (err != cudaSuccess) {
         std::cerr << "CUDA error at " << file << ":" << line << " : "
@@ -10,4 +12,15 @@ void checkCudaErrors(cudaError_t err, const char* file, int line) {
     }
 }
 #define CUDA_CHECK(val) checkCudaErrors((val), __FILE__, __LINE__)
+
+// Custom assertion that verifies if the input poiner points to GPU memory. Exits program on failure via hardcoded CUDA_CHECK call
+template <typename T>
+void assert_device_pointer(T* ptr) {
+    cudaPointerAttributes attr;
+    CUDA_CHECK(cudaPointerGetAttributes(&attr, d_inputs));
+    
+    if (attr.type != cudaMemoryTypeDevice) {
+	CUDA_CHECK(cudaErrorInvalidDevice);
+    }
+}
 
