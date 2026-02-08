4993c92d41264671c0f8343b1097804aea44502a
lizardo-andres12 <lizardo.andres12@gmail.com>
2026-02-08 00:46:40 -0500

    [kernels] update size_t to uint32_t to reduce L1 GPU cache load

--- include/kernels/curandom.cuh
+++ include/kernels/curandom.cuh
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <cstdint>
 #include <curand_kernel.h>
 #include <cuda_runtime.h>
 
@@ -9,6 +10,6 @@ namespace tl::kernels {
     __global__ void init_curand_state(curandState* state, unsigned long long seed);
 
     // Weight and bias random number initializer kernel
-    __global__ void init_random_weights(curandState* state, float* buf, const size_t size);
+    __global__ void init_random_weights(curandState* state, float* buf, const uint32_t size);
 }
 
--- src/kernels/curandom.cu
+++ src/kernels/curandom.cu
@@ -1,13 +1,11 @@
 #include "kernels/curandom.cuh"
 
-using namespace tl::kernels;
-
-__global__ void init_curand_state(curandState *state, unsigned long long seed) {
+__global__ void tl::kernels::init_curand_state(curandState *state, unsigned long long seed) {
     unsigned int idx = threadIdx.x + blockIdx.x * blockDim.x;
     curand_init(seed, idx, 0, &state[idx]);
 }
 
-__global__ void init_random_weights(curandState* state, float *buf, const int size) {
+__global__ void tl::kernels::init_random_weights(curandState* state, float* buf, const uint32_t size) {
     unsigned int idx = threadIdx.x + blockIdx.x * blockDim.x;
     if (idx >= size) {
 	return;
